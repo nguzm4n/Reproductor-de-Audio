e3bd57a743ee6d654349451f6170c2b364ceada2
Nicolás Guzmán <n3kguzman@gmail.com>
2024-04-04 18:09:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import { useState, useRef } from 'react'
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { TbPlayerPlayFilled, TbPlayerPauseFilled, TbPlayerSkipBack, TbPlayerSkipForward  } from "react-icons/tb";
+
+
+function App() {
+
+
+const [songs, setSongs] = useState([
+  {
+    "id": 1,
+    "name": "Mario Castle",
+    "url": "/sound/files/mario/songs/castle.mp3",
+    "category": "category"
+  },
+  {
+    "id": 2,
+    "name": "Mario Star",
+    "url": "/sound/files/mario/songs/hurry-starman.mp3",
+    "category": "category"
+  },
+  {
+    "id": 3,
+    "name": "Mario Overworld",
+    "url": "/sound/files/mario/songs/overworld.mp3",
+    "category": "category"
+  },
+  {
+    "id": 4,
+    "name": "Mario Stage 1",
+    "url": "/sound/files/mario/songs/stage1.mp3",
+    "category": "category"
+  },
+  {
+    "id": 5,
+    "name": "Mario Stage 2",
+    "url": "/sound/files/mario/songs/stage2.mp3",
+    "category": "category"
+  }
+
+])
+
+const chosen = useRef(null)
+const audioRef = useRef(null)
+
+const play = (url, index) => {
+  const fullUrl = `https://playground.4geeks.com${url}`
+  audioRef.current.src = fullUrl
+  audioRef.current.play()
+  chosen.current = index
+}
+
+const continuePlay = () => {
+  audioRef.current.play()
+}
+const pause = () => {
+  audioRef.current.pause()
+}
+const forward = () => {
+ 
+if(chosen.current < songs.length) {
+  chosen.current++
+}
+}
+const backward = () => {
+
+}
+
+  return (
+    <>
+      <div className='App'>
+      <ul className='list-group'>
+        {songs.map((song, index) => {
+         return <li key={song.id} className='list-group-item list-group-item-action' 
+         onClick={() => play(song.url , index)}>
+            {song.name}
+          </li>
+        })}
+        </ul>
+      </div>
+    <audio ref={audioRef} controls />
+    <TbPlayerSkipBack 
+    className='back'/>
+    <TbPlayerPlayFilled 
+    className='play'
+    onClick={continuePlay}/>
+    <TbPlayerPauseFilled 
+    className='pause'
+    onClick={pause}/>
+    <TbPlayerSkipForward 
+    className='forward'
+    onClick={forward}/>
+    </>
+  )
+}
+
+export default App
